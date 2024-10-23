824692e61c13d473d7f1c0b7496c5a5a8e080c30
normalset <128402298+normalset@users.noreply.github.com>
2024-10-23 17:18:55 +0200

    Assignment 5
    Il laboratorio di Informatica del Polo Marzotto è utilizzato da tre tipi di utenti, studenti, tesisti e professori ed ogni utente deve fare una richiesta al tutor per accedere al laboratorio. I computers del laboratorio sono numerati da 1 a 20. Le richieste di accesso sono diverse a seconda del tipo dell'utente:
    
         i professori accedono in modo esclusivo a tutto il laboratorio, poichè hanno necessità di utilizzare tutti i computers per effettuare prove in rete.
        i tesisti richiedono l'uso esclusivo di un solo computer, identificato dall'indice i, poichè su quel computer è istallato un particolare software necessario per lo sviluppo della tesi.
        gli studenti richiedono l'uso esclusivo di un qualsiasi computer
    
    I professori hanno priorità su tutti nell'accesso al laboratorio, i tesisti hanno priorità sugli studenti. Nessuno però può essere interrotto mentre sta usando un computer. Scrivere un programma JAVA che simuli il comportamento degli utenti e del tutor. Il programma riceve in ingresso il numero di studenti, tesisti e professori che utilizzano il laboratorio ed attiva un thread per ogni utente. Ogni utente accede k volte al laboratorio, con k generato casualmente. Simularebl'intervallo di tempo che intercorre tra un accesso ed il successivo e l'intervallo di permanenza in laboratorio mediante il metodo sleep della classe Thread. Il tutor deve coordinare gli accessi al laboratorio. Il programma deve terminare quando tutti gli utenti hanno completato i loro accessi al laboratorio.
    Simulare gli utenti con dei thread e incapsulare la logica di gestione del laboratorio all'interno di un monitor.

---
+++ Assignment5_LaboratorioPc.java
@@ -0,0 +1,122 @@
+import java.util.Random ;
+
+class Laboratorio{
+    private int numComputers = 20 ;
+    // false = libero , true = occupato
+    private boolean[] computers = new boolean[numComputers] ;
+
+    //Richieste di accesso
+    public synchronized void accediProfessore() throws InterruptedException {
+        while(isLabOccupato()){
+            wait() ; //aspetto che il lab si liberi
+        }
+        System.out.println("Professore accede");
+        Thread.sleep(2000) ;
+        System.out.println("Professore logout");
+        notifyAll();
+    }
+
+    public synchronized void accediTesista(int computerIndex) throws InterruptedException {
+        // true = occupato
+        while(computers[computerIndex]){
+            wait() ;
+        }
+        //occupa il computer, lavora e rilascia
+        computers[computerIndex] = true;
+        System.out.println("Tesista accede al pc: "+computerIndex);
+        Thread.sleep(2000) ;
+        System.out.println("Tesista logout dal pc: "+computerIndex);
+        computers[computerIndex] = false;
+        notifyAll();
+    }
+
+    public synchronized void accediStudente() throws InterruptedException {
+        //cerca un pc libero
+        int freePc = getFreePc();
+        while(freePc == -1){
+            wait() ;
+            freePc = getFreePc();
+        }
+
+        computers[freePc] = true;
+        System.out.println("Studente accede al pc: "+freePc);
+        Thread.sleep(2000) ;
+        System.out.println("Studente logout dal pc: "+freePc);
+        computers[freePc] = false;
+        notifyAll();
+    }
+
+    // se c'e' almeno un computer libero il lab non e' occupato
+    public boolean isLabOccupato(){
+        for(boolean computer : computers){
+            if(computer == false) return false ;
+        }
+        return true ;
+    }
+
+    public int getFreePc() {
+        for(int i = 0 ; i < 20 ; i++){
+            if(computers[i] == false){
+                return i ;
+            }
+        }
+        return -1 ;
+    }
+}
+
+class Utente implements Runnable{
+    private Laboratorio laboratorio ;
+    private String tipo ;
+    private int computerIndex ;
+    private int accessCount ;
+
+    public Utente(Laboratorio laboratorio, String tipo , int computerIndex){
+        this.laboratorio = laboratorio ;
+        this.tipo = tipo ;
+        this.computerIndex = computerIndex ;
+        this.accessCount = new Random().nextInt(3) + 1 ; //accedono da 1 a 3 volte
+    }
+
+    public void run(){
+        try{
+            for(int i = 0 ; i < accessCount ; i++){
+                switch (tipo){
+                    case "professore":
+                        laboratorio.accediProfessore();
+                        break;
+                    case "tesista":
+                        laboratorio.accediTesista(computerIndex);
+                        break;
+                    case "studente":
+                        laboratorio.accediStudente();
+                        break;
+                }
+                Thread.sleep(new Random().nextInt(2000) + 1000) ;
+            }
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+
+public class Assignment5_LaboratorioPc {
+    public static void main(String[] args) {
+        Laboratorio laboratorio = new Laboratorio() ;
+
+        //Professori
+        for(int i = 0 ; i < 2 ; i++){
+            new Thread(new Utente(laboratorio, "professore", -1)).start();
+        }
+        //Tesisti
+        for(int i = 0 ; i < 3 ; i++){
+            new Thread(new Utente(laboratorio, "tesista", new Random().nextInt(20))).start();
+        }
+
+        //Studenti
+        for(int i = 0 ; i < 3 ; i++){
+            new Thread(new Utente(laboratorio, "studente", new Random().nextInt(20))).start();
+        }
+    }
+}
